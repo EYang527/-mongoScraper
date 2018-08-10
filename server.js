03d242ec4386fa376e05928e2d84980f811f04de
Eddie77 <edisonyang527@gmail.com>
2018-08-10 07:21:23 -0500

    change port for heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -34,7 +34,10 @@ app.engine("handlebars",exphbs({defaultLayout:"main"}));
 app.set("view engine","handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/hackersNews");
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hackersNews";
+mongoose.Promise= Promise;
+mongoose.connect(MONGODB_URI);
 
 
 
